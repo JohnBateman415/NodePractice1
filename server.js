e44326802bb5eaaf73e36ae3cf9cdf966eca1a2c
John Bateman <jbateman415@gmail.com>
2018-07-10 19:17:12 +0000

    added lodash and fs examples

--- server.js
+++ server.js
@@ -1,84 +1,23 @@
-//
-// # SimpleServer
-//
-// A simple chat server using Socket.IO, Express, and Async.
-//
-var http = require('http');
-var path = require('path');
+var _ = require ("lodash");
+var fs = require("fs");
 
-var async = require('async');
-var socketio = require('socket.io');
-var express = require('express');
+var randomNumber = _.random(-6,10);
 
-//
-// ## SimpleServer `SimpleServer(obj)`
-//
-// Creates a new instance of SimpleServer with the following options:
-//  * `port` - The HTTP port to listen on. If `process.env.PORT` is set, _it overrides this value_.
-//
-var router = express();
-var server = http.createServer(router);
-var io = socketio.listen(server);
+var fileContents = fs.readFileSync("server.js","utf-8");
 
-router.use(express.static(path.resolve(__dirname, 'client')));
-var messages = [];
-var sockets = [];
+console.log("random is "+ randomNumber);
 
-io.on('connection', function (socket) {
-    messages.forEach(function (data) {
-      socket.emit('message', data);
-    });
+console.log(fileContents);
+/*
+console.log(" ...................this is server.js file.................");
 
-    sockets.push(socket);
+var str1 = "bigg";
+var str2 = "bad";
+var n1 = 1;
+var n2 = 2;
 
-    socket.on('disconnect', function () {
-      sockets.splice(sockets.indexOf(socket), 1);
-      updateRoster();
-    });
-
-    socket.on('message', function (msg) {
-      var text = String(msg || '');
-
-      if (!text)
-        return;
-
-      socket.get('name', function (err, name) {
-        var data = {
-          name: name,
-          text: text
-        };
-
-        broadcast('message', data);
-        messages.push(data);
-      });
-    });
-
-    socket.on('identify', function (name) {
-      socket.set('name', String(name || 'Anonymous'), function (err) {
-        updateRoster();
-      });
-    });
-  });
-
-function updateRoster() {
-  async.map(
-    sockets,
-    function (socket, callback) {
-      socket.get('name', callback);
-    },
-    function (err, names) {
-      broadcast('roster', names);
-    }
-  );
-}
-
-function broadcast(event, data) {
-  sockets.forEach(function (socket) {
-    socket.emit(event, data);
-  });
-}
-
-server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function(){
-  var addr = server.address();
-  console.log("Chat server listening at", addr.address + ":" + addr.port);
-});
+console.log(n1 + n2);
+console.log("total is: " + n1 + n2);
+console.log(str1 +" " + str2);
+console.log(Number(n1) + Number(n2));
+*/
